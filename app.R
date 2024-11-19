2c3d90b2f1529f53172a95003e99569a928eac9d
guslipkin <gus.lipkin@gmail.com>
2024-11-19 15:52:01 -0700

    prevent timeouts

--- app.R
+++ app.R
@@ -3,5 +3,5 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options("golem.app.prod" = TRUE, "shiny.sessionTimeout" = 0)
+options("golem.app.prod" = TRUE, "shiny.sessionTimeout" = 0, "shiny.autoreload" = FALSE)
 jotto::run_app() # add parameters here (if any)
